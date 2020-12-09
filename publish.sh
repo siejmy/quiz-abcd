fabf16910b2b09f961efb241e2876766e19aca6f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-12-09 18:40:20 +0100

    ci: registry version tagging

--- publish.sh
+++ publish.sh
@@ -7,4 +7,6 @@ BASE_TAG="jedrzejlewandowski/siejmy-quiz-abcd"
 
 docker build -t "${BASE_TAG}" .
 docker tag "${BASE_TAG}" "${BASE_TAG}:latest"
-docker push "${BASE_TAG}"
+docker tag "${BASE_TAG}" "${BASE_TAG}:1.0.0"
+docker push "${BASE_TAG}:latest"
+docker push "${BASE_TAG}:1.0.0"
