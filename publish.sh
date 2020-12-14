de914905479378ecfc25cc7ab83938020c1b250f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-12-15 00:21:55 +0100

    chore: v2.0.0

--- publish.sh
+++ publish.sh
@@ -4,9 +4,10 @@ cd "${DIR}"
 set -e
 
 BASE_TAG="jedrzejlewandowski/siejmy-quiz-abcd"
+VERSION="2.0.0"
 
 docker build -t "${BASE_TAG}" .
 docker tag "${BASE_TAG}" "${BASE_TAG}:latest"
-docker tag "${BASE_TAG}" "${BASE_TAG}:1.1.0"
+docker tag "${BASE_TAG}" "${BASE_TAG}:${VERSION}"
 docker push "${BASE_TAG}:latest"
-docker push "${BASE_TAG}:1.1.0"
+docker push "${BASE_TAG}:${VERSION}"
